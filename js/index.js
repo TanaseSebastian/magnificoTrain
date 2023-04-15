a738b10109258a775f07d174b7ef629d38726c35
TanaseSebastian <80573505+TanaseSebastian@users.noreply.github.com>
2023-04-15 18:51:55 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 let map;
 
 async function initMap() {
+  const myLatLng = { lat: 40.471780, lng: 17.239286 };
   // The location of Taranto
   const position_taranto = { lat: 40.469219, lng: 17.240061 };
 
@@ -15,8 +16,8 @@ async function initMap() {
 
   // The map, centered at Taranto
   map = new Map(document.getElementById("map"), {
-    zoom: 17,
-    center: position_taranto,
+    zoom: 18,
+    center: myLatLng,
     mapId: "DEMO_MAP_ID",
   });
 
@@ -24,7 +25,11 @@ async function initMap() {
   const infoWindow = new google.maps.InfoWindow();
   const icon = {
     url: "./img/start.png", // url
-    scaledSize: new google.maps.Size(50, 50), // scaled size
+    scaledSize: new google.maps.Size(150, 150), // scaled size
+    size: new google.maps.Size(150,150), //size
+    //origin: null, //origin
+    //anchor: null, //anchor
+    //scale: new google.maps.Size(8,8) //scale
     //origin: new google.maps.Point(0,0), // origin
     //anchor: new google.maps.Point(0, 0) // anchor
   };
@@ -55,13 +60,14 @@ async function initMap() {
 
 
   // Create the markers.
-  tourStops.forEach(([position, title], i, icon) => {
+  tourStops.forEach(([position, title], i) => {
     const marker = new google.maps.Marker({
       position,
       map,
       title: `${i + 1}. ${title}`,
       label: `${i + 1}`,
-      optimized: false,
+      //optimized: false,
+      icon:icon,
     });
 
     // Add a click listener for each marker, and set up the info window.
@@ -81,7 +87,7 @@ async function initMap() {
     //icon: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.svgheart.com%2Fproduct%2Ftrain-locomotive-silhouette-svg-file%2F&psig=AOvVaw2wLEWSh-C_TduhBcnYXGo1&ust=1681655724185000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCODJgfKNrP4CFQAAAAAdAAAAABAE",
   //});
 
-  const myLatLng = { lat: 40.471780, lng: 17.239286 };
+
   //const image ="../img/capolinea.png";
   //const image="./img/start.png"
 
@@ -90,6 +96,39 @@ async function initMap() {
     map,
     icon: icon,
   });
+
+  //when the map zoom changes, resize the icon based on the zoom level so the marker covers the same geographic area
+/*
+  google.maps.event.addListener(map, 'zoom_changed', function() {
+    var pixelSizeAtZoom0 = 8; //the size of the icon at zoom level 0
+    var maxPixelSize = 350; //restricts the maximum size of the icon, otherwise the browser will choke at higher zoom levels trying to scale an image to millions of pixels
+
+    var zoom = map.getZoom();
+    var relativePixelSize = Math.round(pixelSizeAtZoom0*Math.pow(2,zoom)); // use 2 to the power of current zoom to calculate relative pixel size.  Base of exponent is 2 because relative size should double every time you zoom in
+
+    if(relativePixelSize > maxPixelSize) //restrict the maximum size of the icon
+      relativePixelSize = maxPixelSize;
+
+    //change the size of the icon
+    marker.setIcon(
+      new google.maps.MarkerImage(
+     marker.getIcon().url, //marker's same icon graphic
+      null,//size
+      null,//origin
+      null, //origin
+      null, //anchor
+      new google.maps.Size(relativePixelSize, relativePixelSize) //changes the scale
+      )
+    );
+  });
+
+  */
+
+
+
+
+
+
   // The marker, positioned at Uluru
   /*const marker = new AdvancedMarkerView({
     map: map,
